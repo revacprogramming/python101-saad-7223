72992657b181c0f01cc95b62d0a46a2f22778602
saad-7223 <saadsyed837@gmail.com>
2022-07-06 20:56:29 +0530

    p

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,15 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+import urllib.request, urllib.parse, urllib.error 
+
+import xml.etree.ElementTree as ET
+url = input('Enter location: ')
+archivo = urllib.request.urlopen(url)
+data = archivo.read()
+print('Retrieving', len(data), 'characters') 
+xml_data = ET.fromstring(data)
+lst = xml_data.findall('.//count')  
+Cont = len(lst)    
+print("Count:", Cont)  
+lista = [int(item.text) for item in lst]   
+print("Sum:", sum(lista))
