79c5041f19c87eb103138c3dae4d5f4530da1d38
saad-7223 <saadsyed837@gmail.com>
2022-07-06 21:46:55 +0530

    p

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -2,15 +2,20 @@
 
 def get_cs():
     """get string input"""
-
+    s=input("Enter a string : ")
+    return s
 
 def cs_to_lot(cs):
     """convert connected string to list of strings"""
-
+    l=cs.split()
+    return l
 
 def lot_to_cs(lot):
     """convert list of strings to connected string"""
-
+    srt=""
+    for i in lot:
+        srt += i
+    return srt
 
 def main():
     cs=get_cs()
