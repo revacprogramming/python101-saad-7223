3ad0f580b4a4f89eab404aa12ffe7cc7a0becb43
saad-7223 <saadsyed837@gmail.com>
2022-07-06 21:32:51 +0530

    p

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,18 +1,20 @@
 
 def add(a, b):
-    pass  # ...
-
+    return a+b
 
 def output(a, b, sum):
-    pass  # ...
+    sum=a+b
+    print("sum : ",sum)
 
+def input_two_numbers():
+    a=int(input("Enter first number : "))
+    b=int(input("Enter second 9number : "))  
+    return a,b  
 
 def main():
     a, b = input_two_numbers()
     sum = add(a, b)
-
     output(a, b, sum)
-
 
 if __name__ == '__main__':
     main()
