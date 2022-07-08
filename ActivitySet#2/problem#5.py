bb5f12adeb2b6f2533b8a33ab6c9f01b9bc35ca2
saad-7223 <saadsyed837@gmail.com>
2022-07-08 23:07:47 +0530

    p

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -2,14 +2,26 @@
 
 def get_cs():
     """get string input"""
-
+    s=input("Enter a string : ")
+    return s
 
 def cs_to_dict(cs):
     """convert connect string to a dictionary"""
-
+    l=cs.split(';');x=[];d={};
+    for i in l:
+        a=i.split('=')
+        x.append(a)
+    for j in range(0,len(x)):
+        d[x[j][0]]=x[j][1]
+    return d    
 
 def dict_to_cs(d):
     """convert a dictionary to connect string"""
+    l=list(d.items())
+    st=""
+    for i in range(0,len(l)):
+        st=st+l[i][0]+'='+l[i][1]+';'
+    return st
 
 
 def main():
