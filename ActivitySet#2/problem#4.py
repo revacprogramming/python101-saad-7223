69e1413674c39114e56008932802a5c2c70acec0
saad-7223 <saadsyed837@gmail.com>
2022-07-08 23:17:01 +0530

    p

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,4 +1,5 @@
-
+'''[('system','s'),('database','d'),('username','u'),('passwd','p')]
+system=s;database=d;username=u;passwd=p'''
 
 def get_cs():
     """get string input"""
@@ -7,14 +8,19 @@ def get_cs():
 
 def cs_to_lot(cs):
     """convert connected string to list of strings"""
-    l=cs.split()
-    return l
+    l=cs.split(';')
+    x=[]
+    for i in l:
+        a=i.split('=')
+        x.append(tuple(a))
+    return x
+       
 
 def lot_to_cs(lot):
     """convert list of strings to connected string"""
     srt=""
-    for i in lot:
-        srt += i
+    for i in range(0,len(lot)):
+        srt=srt+lot[i][0]+'='+lot[i][1]+';'
     return srt
 
 def main():
