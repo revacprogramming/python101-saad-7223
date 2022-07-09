19c186ea513992fface143ff8b1d0d403bfb4f4d
saad-7223 <saadsyed837@gmail.com>
2022-07-09 22:04:30 +0530

    p

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -9,9 +9,3 @@ rte = float(input("Enter rate per hour? "))
 
 p = computepay(hrs, rte)
 print("Pay", p)
-'''
-OUTPUT:
-python ActivitySet#1/problem#05.py
-Enter hours? 14 
-Enter rate per hour? 10
-Pay 140.0'''
