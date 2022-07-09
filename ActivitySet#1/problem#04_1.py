151decef3f8f5d629ffbefa45d9e52c124beda72
saad-7223 <saadsyed837@gmail.com>
2022-07-09 22:05:30 +0530

    p

--- ActivitySet#1/problem#04_1.py
+++ ActivitySet#1/problem#04_1.py
@@ -1,3 +1,14 @@
-# Conditional Execution
-
-hrs = input("Enter hours? ")
+score = input("Enter Score: ")
+s=float(score)
+if(s>=0.9):
+    print("A")
+elif(s>=0.8):
+    print("B")    
+elif(s>=0.7):
+    print("C")    
+elif(s>=0.6):
+    print("D")    
+elif(s<=0.6):
+    print("F")    
+else:
+    print("Enter the score between range of 0.0 and 1.0")
