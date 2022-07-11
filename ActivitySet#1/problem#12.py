1da57e0d6a1c0560aac3cef9ff471ba246358997
saad-7223 <saadsyed837@gmail.com>
2022-07-11 15:05:26 +0530

    p

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,30 +1,13 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
-'''import re
-
-hand = open("dataset/regex12.txt")
-x=list()
-for line in hand:
-    y = re.findall('[0-9]+',line)
-    x = x+y
-
-sum=0
-for k in x:
-    sum = sum + int(k)
-print(sum)
-'''
 import re
-fname = input('Enter file name: ')
-try:
-    fhand = open(fname)
-except:
-    print("File not found!")
-    quit()
-numlist = []
-for line in fhand:
-    num_in_line = re.findall('[0-9]+', line)
-    if len(num_in_line) < 1 : continue
-    for num in num_in_line:
-        numlist.append(int(num))
-print(sum(numlist))
+hand=open('dataset/regex12.txt')
+lst=[]
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('([0-9]+)', line)
+    int_x=list(map(int,x))
+    lst.extend(int_x)
 
+total=sum(lst)
+print('Total:',total)
