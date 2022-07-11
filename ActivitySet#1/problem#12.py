15641c7c90f2407c5ddab1eac6ce3e1dd6b36bee
saad-7223 <saadsyed837@gmail.com>
2022-07-11 14:41:02 +0530

    p

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,8 +1,8 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
-import re
+'''import re
 
-hand = open("dataset/mbox-short.txt")
+hand = open("dataset/regex12.txt")
 x=list()
 for line in hand:
     y = re.findall('[0-9]+',line)
@@ -12,4 +12,19 @@ sum=0
 for k in x:
     sum = sum + int(k)
 print(sum)
- 
+'''
+import re
+fname = input('Enter file name: ')
+try:
+    fhand = open(fname)
+except:
+    print("File not found!")
+    quit()
+numlist = []
+for line in fhand:
+    num_in_line = re.findall('[0-9]+', line)
+    if len(num_in_line) < 1 : continue
+    for num in num_in_line:
+        numlist.append(int(num))
+print(sum(numlist))
+
